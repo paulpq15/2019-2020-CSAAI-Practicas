011939dd6f443a1aef50b857d1cd92e010f298c3
Willians Paul Pico Quiroz <paulpico@f-l3207-pc36.aulas.etsit.urjc.es>
2020-03-04 14:42:16 +0100

    Terminando parte Javascript

--- P2/p2.js
+++ P2/p2.js
@@ -1,9 +1,9 @@
 console.log("Ejecutando JS...");
 
 display = document.getElementById("display")
-suma = document.getElementById("suma")
-igual = document.getElementById("igual")
 clear = document.getElementById("clear")
+borrar = document.getElementById("borrar")
+igual = document.getElementById("igual")
 
 //-- Crear un array con todos los elementos
 //-- de la clase digito
@@ -11,13 +11,26 @@ digito = document.getElementsByClassName("digito")
 
 for (i=0; i<digito.length; i++) {
   digito[i].onclick = (ev) => {
-    display.innerHTML += ev.target.value;
+    if (display.innerHTML == "0"){
+      display.innerHTML = ev.target.value;
+    }else{
+      display.innerHTML += ev.target.value;
+    }
   }
 }
 
-//-- Insertar simbolo de sumar
-suma.onclick = (ev) => {
-  display.innerHTML += ev.target.value;
+//-- Crear un array con todos los elementos
+//-- de la clase operador
+operador = document.getElementsByClassName("operador")
+
+for (i=0; i<operador.length; i++) {
+  operador[i].onclick = (ev) => {
+    if (display.innerHTML == "0"){
+      display.innerHTML = ev.target.value;
+    }else{
+      display.innerHTML += ev.target.value;
+    }
+  }
 }
 
 //-- Evaluar la expresion
@@ -28,4 +41,16 @@ igual.onclick = () => {
 //-- Poner a cero la expresion
 clear.onclick = () => {
   display.innerHTML = "0";
+}
+
+//-- Borrar el ultimo numero u operador
+//-- introducido en el array
+borrar.onclick = () => {
+  if (display.innerHTML == "0"){
+    display.innerHTML = "0";
+  }else if (display.innerHTML == ""){
+    display.innerHTML = "0";
+  }else{
+    display.innerHTML = display.innerHTML.slice(0,-1)
+  }
 }

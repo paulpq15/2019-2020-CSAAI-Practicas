41ed0e6c7b38d92f7d0daa67cfca41cbe1dbd34f
Willians Paul Pico Quiroz <paulpico@f-l3207-pc36.aulas.etsit.urjc.es>
2020-03-04 14:03:10 +0100

    Empezamos con la parte de javascript

---
+++ P2/p2.js
@@ -0,0 +1,31 @@
+console.log("Ejecutando JS...");
+
+display = document.getElementById("display")
+suma = document.getElementById("suma")
+igual = document.getElementById("igual")
+clear = document.getElementById("clear")
+
+//-- Crear un array con todos los elementos
+//-- de la clase digito
+digito = document.getElementsByClassName("digito")
+
+for (i=0; i<digito.length; i++) {
+  digito[i].onclick = (ev) => {
+    display.innerHTML += ev.target.value;
+  }
+}
+
+//-- Insertar simbolo de sumar
+suma.onclick = (ev) => {
+  display.innerHTML += ev.target.value;
+}
+
+//-- Evaluar la expresion
+igual.onclick = () => {
+  display.innerHTML = eval(display.innerHTML);
+}
+
+//-- Poner a cero la expresion
+clear.onclick = () => {
+  display.innerHTML = "0";
+}

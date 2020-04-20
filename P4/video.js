6dbe3f7a09e564fe614f84f8ce58ed6052552277
paulpq15 <paul.0515@hotmail.com>
2020-04-20 17:29:04 +0200

    New version

---
+++ P4/video.js
@@ -0,0 +1,51 @@
+console.log("Ejecutando JS...");
+
+//----- Obtener elemento de video y configurarlo
+const video1 = document.getElementById("video1")
+video1.width=200;  //-- Tamaño de la pantalla de video
+video1.height=100;
+
+//----- Obtener elemento de video y configurarlo
+const video2 = document.getElementById("video2")
+video2.width=200;  //-- Tamaño de la pantalla de video
+video2.height=100;
+
+//----- Obtener elemento de video y configurarlo
+const video1 = document.getElementById("video3")
+video3.width=200;  //-- Tamaño de la pantalla de video
+video3.height=100;
+
+//-- Imagen estática a mostrar cuando el video no
+//-- ha arrancado
+video1.poster="https://github.com/myTeachingURJC/2019-2020-CSAAI/raw/master/L10/test.png";
+video2.poster="https://github.com/myTeachingURJC/2019-2020-CSAAI/raw/master/L10/test.png";
+video3.poster="https://github.com/myTeachingURJC/2019-2020-CSAAI/raw/master/L10/test.png";
+
+//-- Obtener los botones
+const play = document.getElementById("play")
+const stop = document.getElementById("stop")
+
+//-- Función de retrollamada del botón de ver
+play.onclick = () => {
+  console.log("Click!");
+  video1.src="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente1.mp4"
+  video1.play();
+  video2.src="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente2.mp4"
+  video2.play();
+  video3.src="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente3.mp4"
+  video3.play();
+};
+
+//-- Funcion de retrollamada del boton de parar
+stop.onclick = () => {
+  video1.pause();
+  video2.pause();
+  video3.pause();
+
+  //-- Quitar la fuente de video, para que se muestre la
+  //-- imagen definida en el atributo poster
+  video1.src=null;
+  video2.src=null;
+  video3.src=null;
+
+}

119c1269bdb01c6f1cf8158ff523a3ca1398c40e
paulpq15 <paul.0515@hotmail.com>
2020-04-23 19:35:12 +0200

    Siguiendo pasos

--- P5/filtro.js
+++ P5/filtro.js
@@ -11,8 +11,6 @@ const ctx = canvas.getContext('2d');
 //-- que esté totalmente cargada
 img.onload = function () {
 
-  console.log("Imagen cargada");
-
   //-- Se establece como tamaño del canvas el mismo
   //-- que el de la imagen original
   canvas.width = img.width;
@@ -21,6 +19,25 @@ img.onload = function () {
   //-- Situar la imagen original en el canvas
   //-- No se han hecho manipulaciones todavia
   ctx.drawImage(img, 0,0);
+
+  //-- Obtener la imagen del canvas en pixeles
+  let imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+  //-- Obtener el array con todos los píxeles
+  let data = imgData.data
+
+  //-- Obtener el numero total de elementos en el array
+  console.log("Tamaño de data: " + data.length)
+
+  //-- El número total de pixeles es la altura por la anchura
+  npixels = canvas.width * canvas.height
+  console.log("Anchura (en pixeles): " + canvas.width)
+  console.log("Altura (en pixeles): " + canvas.height)
+  console.log("Pixeles totales: " + npixels)
+
+  //-- Puesto que cada pixel ocupa 4 bytes, el array de píxeles
+  //-- tiene un tamaño de 4 * numero de pixeles
+  console.log("Total de datos de la imagen: " + npixels * 4)
 };
 
 console.log("Fin...");
